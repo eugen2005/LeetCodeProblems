fad23ccc62c00734d3c7ffa658e1a8b07698fd2b
Evgheni <eugen2005@gmail.com>
2019-03-12 17:26:49 +0200

    Update 176. Second Highest Salary.sql
    added new solutions

--- database/176. Second Highest Salary.sql
+++ database/176. Second Highest Salary.sql
@@ -14,3 +14,16 @@ select max(salary) SecondHighestSalary from
 (select salary, dense_rank() over (order by salary desc) N
 from employee)
 where N = 2;
+
+/* Solution from forum*/
+SELECT max(Salary)
+FROM Employee
+WHERE Salary < (SELECT max(Salary) FROM Employee)
+
+/* Accepted solution */
+SELECT DISTINCT
+    Salary AS SecondHighestSalary
+FROM
+    Employee
+ORDER BY Salary DESC
+LIMIT 1 OFFSET 1
