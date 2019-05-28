e40000273955dbff8232ce7d7d035b805aa9204a
Evgheni <eugen2005@gmail.com>
2019-05-28 16:57:05 +0300

    Signed-off-by: Evgheni <eugen2005@gmail.com>

--- database/180. Consecutive Numbers.sql
+++ database/180. Consecutive Numbers.sql
@@ -4,3 +4,24 @@ from
 from Logs l) t
 group by seqnum, ConsecutiveNums
 having count(idn) > 2;
+
+/* recomended solution */
+SELECT distinct l1.num
+FROM
+    Logs l1,
+    Logs l2,
+    Logs l3
+WHERE
+    l1.id = l2.id - 1
+    AND l2.id = l3.id - 1
+    AND l1.num = l2.num
+    AND l2.num = l3.num;
+
+/* solution from forum */
+SELECT DISTINCT num
+FROM
+(
+SELECT num,LEAD(num) OVER(ORDER BY id) AS lead, LAG(num) OVER (ORDER BY id) AS lag
+FROM Logs
+)t
+WHERE num=lead and num=lag;
